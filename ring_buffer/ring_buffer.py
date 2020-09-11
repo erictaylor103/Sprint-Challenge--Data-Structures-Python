3c2026534fb638c7baf2d7483e289fc724fd0b76
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-09-11 12:54:13 -0400

    complete ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.current_index = 0 #create a current index counter so we know ar what index we are currently
+        self.capacity = capacity #
+        self.buffer = []#create an empty buffer to append the items
 
     def append(self, item):
-        pass
+        self.item = item
+        if len(self.buffer) == self.capacity:
+            #set the item to be on the current_index of the buffer list
+            #this replaces the item in the current_index for the new item in the buffer list
+            self.buffer[self.current_index] = item
+            
+            print(f"New Item: {item}")
+            print(f"Buffer: {self.buffer}")
+            print(f"Current Index: {self.current_index} \n")
+        
+        else:
+            self.buffer.append(item)
+        self.current_index = (self.current_index + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.buffer
